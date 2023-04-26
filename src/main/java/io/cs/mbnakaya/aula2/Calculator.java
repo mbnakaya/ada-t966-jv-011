983084879ce726b06091cdf95e2331489c7868e7
mbnakaya <mbnakaya25@gmail.com>
2023-04-26 17:00:32 -0300

    create calculator example class

--- src/main/java/io/cs/mbnakaya/aula2/Calculator.java
+++ src/main/java/io/cs/mbnakaya/aula2/Calculator.java
@@ -1,5 +1,7 @@
 package io.cs.mbnakaya.aula2;
 
+import java.util.List;
+
 class Calculator {
 
     protected Integer fibonacci(Integer index) {
@@ -13,9 +15,25 @@ class Calculator {
         return n3;
     }
 
-    // calcula baschara
+    protected Double squareArea(Double side) {
+        return Math.pow(side, 2);
+    }
 
-    // calcula raiz quadrada
+    protected List<Double> bhaskara(Integer a, Integer b, Integer c) {
+        double delta, x1, x2;
+        delta = (b * b) + (-4 * (a * c));
 
-    // calcula area
+        if (delta >= 0) {
+
+            x1 = ((-(b) + Math.sqrt(delta)) / 2 * a);
+            x2 = ((-(b) - Math.sqrt(delta)) / 2 * a);
+
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+
+        } else {
+            throw new DeltaHasNoSqrtException();
+        }
+        return List.of(x1, x2);
+    }
 }
---
+++ src/main/java/io/cs/mbnakaya/aula2/DeltaHasNoSqrtException.java
@@ -0,0 +1,3 @@
+package io.cs.mbnakaya.aula2;
+
+public class DeltaHasNoSqrtException extends RuntimeException { }
